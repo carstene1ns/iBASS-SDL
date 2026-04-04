sev-/scummvm-new

ENGINES: Replace many printfs by warning/debug/debugN
svn-id: r54031

engines/sky/disk.cpp
@@ -59,7 +59,7 @@ Disk::Disk() {
 	if (!_dataDiskHandle->isOpen())
 		error("Error opening %s", dataFilename);
 
-	printf("Found BASS version v0.0%d (%d dnr entries)\n", determineGameVersion(), _dinnerTableEntries);
+	debug("Found BASS version v0.0%d (%d dnr entries)", determineGameVersion(), _dinnerTableEntries);
 
 	memset(_buildList, 0, 60 * 2);
 	memset(_loadedFilesList, 0, 60 * 4);

carstene1ns/iBASS-SDL

sky/disk.cpp
@@ -77,6 +77,8 @@ Disk::Disk() {
 			FileEntry{filenum, offset, size, compressed_size});
 	}
 
+	debug("Found iBASS version v0.0%d (%d data entries)", determineGameVersion(), numFiles);
+
 	memset(_buildList, 0, 60 * 2);
 	memset(_loadedFilesList, 0, 60 * 4);
 }